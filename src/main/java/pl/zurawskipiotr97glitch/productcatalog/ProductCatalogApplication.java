4c0854cd170d6ff46d2d3188fa7cbfe22242f394
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-03-12 15:18:09 +0100

    Initial commit

---
+++ src/main/java/pl/zurawskipiotr97glitch/productcatalog/ProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package pl.zurawskipiotr97glitch.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApplication.class, args);
+	}
+
+}
