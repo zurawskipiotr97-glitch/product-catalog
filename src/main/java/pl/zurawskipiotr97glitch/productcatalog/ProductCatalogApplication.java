5f85534e7b4677a71ed20fd38bf2438d9c7fbd89
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-03-12 15:27:51 +0100

    app properties and liquibase setup

--- src/main/java/pl/zurawskipiotr97glitch/productcatalog/ProductCatalogApplication.java
+++ src/main/java/pl/zurawskipiotr97glitch/productcatalog/ProductCatalogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductCatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductCatalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogApplication.class, args);
+    }
 
 }
